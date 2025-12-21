ec7e1465cbcb7f953b7ab71f0fd19106f0022f66
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2025-12-21 22:05:40 +0700

    feat: finish 8_programming_design_catering_contract.py

---
+++ basic_python/unit_1/8_programming_design_catering_contract.py
@@ -0,0 +1,16 @@
+client = input("Enter client name: ")
+caterer = "King Catering"
+
+contract = (
+    "This Catering Contract is entered into between"
+    + " " + caterer + " ('Caterer') and " + client
+    + " ('Client') (together, 'Parties') and sets"
+    + " forth the agreement between"
+    + " the Parties related to catering services to be"
+    + " provided by the Caterer for Client for the event"
+    + " identified in this Contract."
+)
+
+print("Catering Contract (" + caterer + ")")
+print("----------")
+print(contract)
