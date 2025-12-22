3d47a1414b476398c9cf57b316d02b6361c9969c
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2025-12-22 09:57:17 +0700

    feat: finish 10_type_casting.py

---
+++ basic_python/unit_1/10_type_casting.py
@@ -0,0 +1,50 @@
+# What is type casting?
+
+# Type casting means converting one data type into another.
+
+# Common Python type-casting functions:
+
+# int() → converts to integer
+
+# float() → converts to float
+
+# str() → converts to string
+
+# bool() → converts to boolean
+
+# Example 1:
+
+# new_score = float(input("Enter a quiz score: "))
+# avg_score = (94 + 91 + 83.5) / 3
+# print("My average quiz score is " + str(avg_score))
+
+# Example 2: String → Integer
+
+# x = "10"
+# y = int(x)
+
+# print(y)
+# print(type(y))
+
+# Example 3: Integer → Float
+
+# x = 7
+# y = float(x)
+
+# print(y)
+# print(type(y))
+
+# Example 4: Number → String
+
+# x = 25
+# y = str(x)
+
+# print("My age is " + y)
+
+# Example 5: Value → Boolean
+
+# print(bool(1))
+# print(bool(0))
+# print(bool(""))
+# print(bool("hello"))
+
