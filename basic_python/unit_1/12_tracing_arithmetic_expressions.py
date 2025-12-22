554b12d4d14dde7bcbe2ffbe6ee891d9a2feb9d8
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2025-12-22 16:06:57 +0700

    feat: finish 12_tracing_arithmetic_expressions.py

---
+++ basic_python/unit_1/12_tracing_arithmetic_expressions.py
@@ -0,0 +1,7 @@
+# 1.
+# num_servings = int(input("How many servings? "))
+# spices = num_servings * (6 - 2) / 3
+
+# # The spice jar only has 7 grams of nutmeg left.
+# spices = min(spices, 7) + max(num_servings, 4)
+# print(str(round(spices + 3, 2)) + " grams")
