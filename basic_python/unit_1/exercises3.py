9dae64787a1ae9a9e628386ca1a5a4a6058f7f5c
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2025-12-29 18:44:18 +0700

    feat: finish exercises3.py

---
+++ basic_python/unit_1/exercises3.py
@@ -0,0 +1,47 @@
+# *due
+
+#lx0x
+
+# Create nested folders house/room/bathroom.
+
+#lx0r
+
+# 1. Create a house directory/folder
+# 2. Remove house directory/folder
+
+# 3. Create hotel, motel, and guesthouse folders
+# 4. Remove hotel, motel and guesthouse folders with one line of command
+
+# #lx0m
+
+# 1. Create a greeting.txt file and write "hello world!" in it.
+# 2. Copy greeting.txt to hello.txt
+
+# #lx0q
+
+# 1. Create a room folder.
+# 2. Create a bathroom.md file inside room folder.
+# 3. Remove room folder.
+
+# #lx0s
+
+# 1. What does "cp" command stand for?
+# 2. Why do we use it?
+
+# #lx0o
+
+# 1. Create a house folder.
+# 2. Create a room folder inside the house folder.
+
+# 3. Use rmdir ​to remove house folder then read the Error Message.
+# 4. Use "rm -r" to remove house folder.
+
+# #lx0p
+
+# 1. Create a room directory.
+# 2. Create book1.txt book2.txt (Write "workbook1" in book1.txt, and "workbook2" in book2.txt)
+
+# 3. Copy book1.txt and book2.txt to room directory
+# 4. Check book1.txt and book2.txt
+
+# *bookmark
